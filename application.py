d441d018343031f2016cad78cfe224c5c6fdb508
aptperson <aptperson@gmail.com>
2020-08-17 10:49:05 +1000

    moved init of app

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 # application.py
 from src.app_current_month import dash_app 
 
+app = dash_app()
+application = app.server
+
 if __name__ == '__main__':
-    app = dash_app()
-    application = app.server
     # app.run_server(debug=True)
     application.run(debug=True, port=8080)
