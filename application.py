1ae4ca512cd09ed82d80ca962239c99f451c4a99
aptperson <aptperson@gmail.com>
2020-08-17 10:44:13 +1000

    changed app name to match eb

--- application.py
+++ application.py
@@ -3,4 +3,6 @@ from src.app_current_month import dash_app
 
 if __name__ == '__main__':
     app = dash_app()
-    app.run_server(debug=True)
+    application = app.server
+    # app.run_server(debug=True)
+    application.run(debug=True, port=8080)
